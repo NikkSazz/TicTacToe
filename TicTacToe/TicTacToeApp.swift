a25b5bc4b052fd94e6fc2ad74668b6ecda0620de
Nikolai Sazonov <nsazonov9@gmail.com>
2025-09-26 15:41:38 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Dmitry Sazonov on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
